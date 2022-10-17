07798a9ebbfbb567488a665dc3a1ad20a8d3c86c
guilisboa07 <guilhermy.lisboa05@gmail.com>
2022-10-17 08:54:40 -0300

    Exercicio 17 - Realização

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { ExclusaoViagemComponent } from './exclusao-viagem/exclusao-viagem.component';
+import { HomeComponent } from './home/home.component';
+import { DetalhesViagemComponent } from './detalhes-viagem/detalhes-viagem.component';
+import { ListagemViagemComponent } from './listagem-viagem/listagem-viagem.component';
+import { CadastroViagemComponent } from './cadastro-viagem/cadastro-viagem.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+  {path: 'cadastro-viagem', component: CadastroViagemComponent},
+  {path: 'detalhes-viagem/:id', component: DetalhesViagemComponent},
+  {path: 'exclusao-viagem/:id', component: ExclusaoViagemComponent},
+  {path: '', component: ListagemViagemComponent}
+
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
